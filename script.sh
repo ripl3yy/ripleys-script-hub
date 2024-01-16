fce5d8b54dd482d2cb82ccaea2f2553c972f550e
Ripley <72573511+ripl3yy@users.noreply.github.com>
2024-01-16 10:21:34 -0600

    spacing actually fixed

--- script.sh
+++ script.sh
@@ -29,11 +29,11 @@ case $CHOICE in
       echo "########################################"
       echo "     Installing packages via pacman     "
       echo "########################################"
-			sleep .5
-			sudo pacman -Syyu
-			sleep 2
-			sudo pacman -S --noconfirm xf86-video-amdgpu libvdpau-va-gl vulkan-swrast libva-vdpau-driver discord flatpak-kcm discover packagekit-qt5 flatpak steam btop
-			sleep 1
+            sleep .5
+            sudo pacman -Syyu
+            sleep 2
+            sudo pacman -S --noconfirm xf86-video-amdgpu libvdpau-va-gl vulkan-swrast libva-vdpau-driver discord flatpak-kcm discover packagekit-qt5 flatpak steam btop
+            sleep 1
       echo "###########################################"
       echo "  Done! Make sure to restart your system!  "
       echo "###########################################"
@@ -59,14 +59,14 @@ case $CHOICE in
       echo "##########################################"
       echo "           Cloning Wallpapers             "
       echo "##########################################"
-			sleep 1
-			cd ~
-			sleep .5
-			git clone https://github.com/ripl3yy/wallpapers
-			sleep .5
-			mv ~/wallpapers ~/Pictures
-			sleep .5
-			rm -rf ~/Pictures/wallpapers/README.md
+            sleep 1
+            cd ~
+            sleep .5
+            git clone https://github.com/ripl3yy/wallpapers
+            sleep .5
+            mv ~/wallpapers ~/Pictures
+            sleep .5
+            rm -rf ~/Pictures/wallpapers/README.md
       echo "############################################################"
       echo "   Done! Wallpapers can be found in your Pictures folder!   "
       echo "############################################################"
@@ -79,15 +79,15 @@ case $CHOICE in
       echo "##########################################"
       echo "        Installing Starship Prompt        "
       echo "##########################################"
-			sleep 1
-			curl -sS https://starship.rs/install.sh | sh
-			sleep 2
-			if ! [ -e ~/.bashrc ]; then
-			    touch ~/.bashrc
-			fi
-			sleep 2
-			echo "eval \"\$(starship init bash)\"" >> ~/.bashrc
-			sleep 1
+            sleep 1
+            curl -sS https://starship.rs/install.sh | sh
+            sleep 2
+            if ! [ -e ~/.bashrc ]; then
+                touch ~/.bashrc
+            fi
+            sleep 2
+            echo "eval \"\$(starship init bash)\"" >> ~/.bashrc
+            sleep 1
       echo "######################################"
       echo "   Done! Close terminal and reopen!   "
       echo "######################################"
@@ -100,27 +100,27 @@ case $CHOICE in
       echo "################################################################"
       echo "  Installing packages, getting wallpapers, setting up Starship  "
       echo "################################################################"
-			sleep .5
-			sudo pacman -Syyu
-			sleep 2
-			sudo pacman -S --noconfirm xf86-video-ati vulkan-radeon lib32-vulkan-radeon libvdpau-va-gl vulkan-swrast libva-vdpau-driver discord flatpak-kcm discover packagekit-qt5 flatpak steam btop
-			sleep 1
-			git clone https://github.com/ripl3yy/wallpapers
-			sleep .5
-			mv ~/wallpapers ~/Pictures
-			sleep .5
-			rm -rf ~/Pictures/wallpapers/README.md
-			sleep 2
-			curl -sS https://starship.rs/install.sh | sh
-			sleep 2
-			if ! [ -e ~/.bashrc ]; then
-			    touch ~/.bashrc
-			fi
-			sleep 2
-			echo "eval \"\$(starship init bash)\"" >> ~/.bashrc
-			sleep 1
-			flatpak install flathub com.spotify.Client com.github.tchx84.Flatseal io.github.alainm23.planify md.obsidian.Obsidian com.heroicgameslauncher.hgl
-			sleep 1
+            sleep .5
+            sudo pacman -Syyu
+            sleep 2
+            sudo pacman -S --noconfirm xf86-video-ati vulkan-radeon lib32-vulkan-radeon libvdpau-va-gl vulkan-swrast libva-vdpau-driver discord flatpak-kcm discover packagekit-qt5 flatpak steam btop
+            sleep 1
+            git clone https://github.com/ripl3yy/wallpapers
+            sleep .5
+            mv ~/wallpapers ~/Pictures
+            sleep .5
+            rm -rf ~/Pictures/wallpapers/README.md
+            sleep 2
+            curl -sS https://starship.rs/install.sh | sh
+            sleep 2
+            if ! [ -e ~/.bashrc ]; then
+                touch ~/.bashrc
+            fi
+            sleep 2
+            echo "eval \"\$(starship init bash)\"" >> ~/.bashrc
+            sleep 1
+            flatpak install flathub com.spotify.Client com.github.tchx84.Flatseal io.github.alainm23.planify md.obsidian.Obsidian com.heroicgameslauncher.hgl
+            sleep 1
       echo "#####################################################################"
       echo "  Done! Restart system and find wallpapers in your Pictures folder!  "
       echo "#####################################################################"
