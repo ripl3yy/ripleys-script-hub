95fb312f2415857ce41748669728ba6fc80b0f5c
Ripley <72573511+ripl3yy@users.noreply.github.com>
2024-07-28 16:18:00 -0500

    Cleaned up code and other major improvements

--- script.sh
+++ script.sh
@@ -1,160 +1,137 @@
 #!/bin/bash
 #set -e
 
-echo
-echo "########################"
-echo "    Checking for git    "
-echo "########################"
-      sleep 1
-      sudo pacman -S --needed git
-      sleep 1
-echo "################################"
-echo "  Done! Running the next step.  "
-echo "################################"
-      sleep 3
-echo
-tput setaf 3
-echo "################################################"
-echo "#       Ripley's Easy Arch Script (REAS)       #"
-echo "################################################"
-tput sgr0
-echo
-echo "Select which to install first."
-echo
-echo "1. Essential packages via pacman"
-echo "2. Essential packages via Flatpak"
-echo "3. Clone wallpapers"
-echo "4. Install Starship prompt"
-echo "5. Install yay."
-echo "6. All of the above (primarily for fresh Arch installs)"
-echo
-echo "Type your selection. To exit, just close this window or press Ctrl + C."
-echo
+# Check for git once
+clear
+echo "Checking for git..."
+sleep 1
+sudo pacman -S --noconfirm --needed git
+echo "Done!"
+sleep 3
+
+# Main menu
+main_menu() {
+    clear
+    tput setaf 4
+    echo "################################################"
+    echo "#       Ripley's Easy Arch Script (REAS)       #"
+    echo "################################################"
+    tput sgr0
+    echo
+    echo "Select which to install first."
+    echo
+    echo "1. Essential packages via pacman"
+    echo "2. Essential packages via Flatpak"
+    echo "3. Clone wallpapers"
+    echo "4. Install Starship prompt"
+    echo "5. Install yay"
+    echo "6. All of the above (primarily for fresh installs)"
+    tput setaf 2
+    echo "Type your selection. To exit, just close this window or press Ctrl + C."
+    tput sgr0
+    echo
+}
 
 while :; do
+    clear
+    main_menu
 
-read CHOICE
+    echo "Please enter your choice:"
+    read CHOICE
 
-case $CHOICE in
+    case $CHOICE in
 
-    1 )
-      echo
-      echo "########################################"
-      echo "     Installing packages via pacman     "
-      echo "########################################"
-            sleep .5
+      # Install via pacman
+        1 )
+            clear
+            echo "Installing packages via pacman..."
             sudo pacman -Syyu
             sleep 2
             sudo pacman -S --noconfirm discord flatpak-kcm discover packagekit-qt5 flatpak steam btop neofetch xdg-desktop-portal ffmpegthumbs
-            sleep 1
-      echo "###########################################"
-      echo "  Done! Make sure to restart your system!  "
-      echo "###########################################"
-            exit
-        ;;
+            echo "Packages installed successfully!"
+            sleep 3
+            ;;
 
-    2 )
-      echo
-      echo "#########################################"
-      echo "     Installing packages via Flatpak     "
-      echo "#########################################"
-            sleep .5
+      # Install via Flatpak
+        2 )
+            clear
+            echo "Installing packages via Flatpak..."
             flatpak install flathub com.spotify.Client com.github.tchx84.Flatseal com.heroicgameslauncher.hgl com.github.unrud.VideoDownloader org.vinegarhq.Vinegar net.davidotek.pupgui2 io.mrarm.mcpelauncher com.obsproject.Studio
             sleep 1
-      echo "###########################################"
-      echo "  Done! Make sure to restart your system!  "
-      echo "###########################################"
-            exit
-        ;;
+            echo "Flatpaks installed successfully!"
+            sleep 3
+            ;;
 
-    3 )
-      echo
-      echo "##########################################"
-      echo "           Cloning Wallpapers             "
-      echo "##########################################"
+      # Clone wallpapers
+        3 )
+            clear
+            echo "Cloning wallpapers..."
             sleep 1
             cd ~ && git clone https://github.com/ripl3yy/wallpapers
-            sleep .5
+            sleep 1
             mv ~/wallpapers ~/Pictures
             sleep .5
             rm -rf ~/Pictures/wallpapers/README.md
-      echo "############################################################"
-      echo "   Done! Wallpapers can be found in your Pictures folder!   "
-      echo "############################################################"
-            exit
+            echo "Wallpapers cloned! Check your pictures folder!"
+            sleep 3
+            ;;
 
-      ;;
-
-          4 )
-      echo
-      echo "##########################################"
-      echo "        Installing Starship Prompt        "
-      echo "##########################################"
-            sleep 1
+      # Install Starship prompt
+        4 )
+            clear
+            echo "Installing Starship prompt..."
             curl -sS https://starship.rs/install.sh | sh
-            sleep 2
+            sleep 1
             if ! [ -e ~/.bashrc ]; then
                 touch ~/.bashrc
             fi
-            sleep 2
-            echo "eval \"\$(starship init bash)\"" >> ~/.bashrc
             sleep 1
-      echo "######################################"
-      echo "   Done! Close terminal and reopen!   "
-      echo "######################################"
-            exit
+            echo 'eval "$(starship init bash)"' >> ~/.bashrc
+            echo "Starship prompt installed! Restart terminal."
+            sleep 3
+            ;;
 
-      ;;
-
-          5 )
-      echo
-      echo "######################"
-      echo "    Installing yay    "
-      echo "######################"
-            sleep 1
+      # Install yay
+        5 )
+            clear
+            echo "Installing yay..."
             pacman -S --needed git base-devel
             cd ~ && git clone https://aur.archlinux.org/yay.git
             sleep 1
             cd yay
             makepkg -si
             sleep 1
-      echo "######################################"
-      echo "   Done! Close terminal and reopen!   "
-      echo "######################################"
-            exit
+            echo "Done! Close terminal and reopen."
+            sleep 3
+            ;;
 
-      ;;
-
-          6 )
-      echo
-      echo "################################################################"
-      echo "  Installing packages, getting wallpapers, setting up Starship  "
-      echo "################################################################"
-            sleep .5
+      # Install everything
+        6 )
+            clear
+            echo "Installing via pacman & flatpak, getting wallpapers, setting up Starship..."
             sudo pacman -Syyu
-            sleep 2
             sudo pacman -S --noconfirm discord flatpak-kcm discover packagekit-qt5 flatpak steam btop neofetch xdg-desktop-portal-gtk ffmpegthumbs kdenlive
             sleep 1
             cd ~ && git clone https://github.com/ripl3yy/wallpapers
-            sleep .5
             mv ~/wallpapers ~/Pictures
-            sleep .5
             rm -rf ~/Pictures/wallpapers/README.md
             sleep 2
             curl -sS https://starship.rs/install.sh | sh
-            sleep 2
+            sleep 0.5
             if ! [ -e ~/.bashrc ]; then
                 touch ~/.bashrc
             fi
-            sleep 2
-            echo "eval \"\$(starship init bash)\"" >> ~/.bashrc
+            sleep 1
+            echo 'eval "$(starship init bash)"' >> ~/.bashrc
             sleep 1
             flatpak install flathub com.spotify.Client com.github.tchx84.Flatseal com.heroicgameslauncher.hgl com.github.unrud.VideoDownloader net.davidotek.pupgui2 io.mrarm.mcpelauncher com.obsproject.Studio
-            sleep 1
-      echo "#####################################################################"
-      echo "  Done! Restart system and find wallpapers in your Pictures folder!  "
-      echo "#####################################################################"
-            exit
-      ;;
-esac
+            echo "Done! Restart system and find wallpapers in your Pictures folder!"
+            sleep 3
+            ;;
+        * )
+            clear
+            echo "Invalid option. Please try again."
+            sleep 2
+            ;;
+    esac
 done
